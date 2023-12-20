7e58d0fc520c6be8eeafb703e899b7bc00e1620e
tomoikey <55743826+tomoikey@users.noreply.github.com>
2023-12-21 01:47:48 +0900

    add tuple

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,11 @@ fn main() {
         array_array: [[i32; 3]; 3],
         array_tuple1: [(u16, String); 2],
         array_tuple2: [(u16, String, usize, usize); 2],
+        tuple: (u8, u8)
     }
 
     println!(
-        "\n{:?}\n{:?}\n{:?}\n{:?}\n{:?}\n{:?}",
-        hello, one, array, array_array, array_tuple1, array_tuple2
+        "\n{:?}\n{:?}\n{:?}\n{:?}\n{:?}\n{:?}\n{:?}",
+        hello, one, array, array_array, array_tuple1, array_tuple2, tuple
     );
 }
